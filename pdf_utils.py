c19ec44a7f2a0ce669f6cbb58e0e279a4ac3a978
Picorims <picorims.contact@gmail.com>
2023-01-07 23:21:47 +0100

    generalise chart creation, add title page

--- pdf_utils.py
+++ pdf_utils.py
@@ -33,3 +33,16 @@ def addTitlePage(pdf: PdfPages, title: str, pageSize: Tuple[float,float]):
     fig.clear()
     fig.text(0.5, 0.5, title, size=48, ha="center")
     pdf.savefig(fig)
+
+
+
+def addFirstPage(pdf: PdfPages, pageSize: Tuple[float,float], playlistName: str, nbTracks: int):
+    """
+    Add a presentation page with the playlist name, and other information
+    """
+    fig = plt.figure(figsize=pageSize)
+    fig.clear()
+    fig.text(0.5, 0.5, "Playlist statistics for:\n" + playlistName, size=48, ha="center")
+    fig.text(0.5, 0.25, f"analyzing {str(nbTracks)} tracks", size=24, ha="center")
+    fig.text(0.5, 0.05, "Made using Spotify Playlist Analyzer, by Picorims.\nThis tool is not affiliated to Spotify.", size=12, ha="center")
+    pdf.savefig(fig)
--- spotify_playlist_analyzer.py
+++ spotify_playlist_analyzer.py
@@ -22,6 +22,7 @@
 
 # Dependencies
 from datetime import datetime
+import math
 import matplotlib as mpl
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
@@ -88,31 +89,6 @@ print("Preparing CSV loading...")
 csvPath = os.path.join(tempDir, "data.csv")
 shutil.copyfile(sys.argv[1], csvPath)
 
-# csvFile = open(csvPath, "r", encoding="utf-8")
-# csvLinesR = csvFile.readlines()
-# csvFile.close()
-# csvFile = open(csvPath, "w", encoding="utf-8")
-# csvLinesW = []
-
-# for line in csvLinesR:
-#     csvLinesW.append(line.replace('","','"|"'))
-
-# csvFile.writelines(csvLinesW)
-# csvFile.close()
-
-
-
-
-
-# Load CSV LEGACY
-
-# Note for numpy for the future:
-# - do not handle string. example: "a,b","c" ==> ["a, b", "c"]. A separator not used anywhere is required.
-# - consider a '#' as a comment character, which means it ignores content after a # on a line.
-
-# genfromtxt handle empty values, using the NULL utf-8 character to disable comments
-# data = np.genfromtxt(csvPath, delimiter="|", dtype=str, encoding="utf-8", comments="�")
-
 
 
 
@@ -171,6 +147,8 @@ def addTitle(title: str):
     """Append a title page to the pdf."""
     pdf_utils.addTitlePage(pdf, title, pdfPageSize)
 
+playlistLength = dataFrame.shape[0]
+pdf_utils.addFirstPage(pdf, pdfPageSize, playlistFileName, playlistLength)
 print("Creating diagrams...")
 
 
@@ -180,31 +158,97 @@ print("Creating diagrams...")
 # POPULARITY (between 0 and 100)
 # ==========
 
-print("- popularity frequency")
-addTitle("Popularity")
+print("- distribution")
+addTitle("Distribution")
 
-# LEGACY WAY OF CALCULATING FREQUENCIES USING PANDAS
-# # Round all values by multiple of 5 to calculate frequency by bins of 5 (0-4 ; 5-9; 10-14; ... ; 95-100)
-# popularitiesByRange = dataFrame[Columns.POPULARITY].apply(lambda x : math_utils.floorByStepsOf(5, x))
-# # Handle 100
-# popularitiesByRange = popularitiesByRange.replace(100, 95)
+def buildDistribution(name: str, distribDataColumn: str, rMin: int, rMax: int, rStep: int, scale: int=1, unit: str="", xLabel: str="", color: str="C0"):
+    """
+    Builds an histogram with the given name,
+    displaying the distribution of values for the given column.
+    Labels contain the number of occurences, and ticks contain
+    the ranges.
+    
+    Parameters:
+    - `name`: name of the figure.
+    - `distribDataColumn`: the dataFrame column to use in `dataFrame`.
+    - `min`, `max`, `step`: range that defines the bins for the histogram (min, max (included!), and step for slicing the whole range in bins).
+    Note that bars will be added for all values below min and above max. 
+    - `scale`: can be used to apply a factor to the ranked column (on a dataframe copy).
+    - `unit`: String to display a unit next to the axis label.
+    - `xlabel`: override the label for the x axis with a custom one, instead of the column name.
+    - `color`: Sets the bars color
+    """
 
-# # Count every bin and store it in a dictiorary
-# popularityFreqs = popularitiesByRange.value_counts().sort_index()
+    distribDf = dataFrame[distribDataColumn]
 
-# Bins
-popBins = list(range(0, 100, 5))
+    # Bins
+    distribBins = np.arange(rMin, rMax+1, rStep)
+    # add min and max values from data to bins,
+    # to show values below min and above max with bars on the sides of the chart
+    maxBin = max(rMax, math.ceil(distribDf.max())) # if the order is broken because (data_max < rMax), hist() will fail
+    minBin = min(rMin, math.floor(distribDf.min()))
+    distribBins = np.append(distribBins, maxBin)
+    distribBins = np.insert(distribBins, 0, minBin)
 
-popFig, popAxes = plt.subplots() #1 row, 1 col
-popFig.set_size_inches(pdfPageSize)
-counts, edges, bars = popAxes.hist(dataFrame[Columns.POPULARITY].to_numpy(), bins=popBins)
-plt.bar_label(bars)
-popAxes.set_title("How popular are the tracks of your playlist ? (between 0 and 100)")
-popAxes.set_xlabel("Popularity")
-popAxes.set_ylabel("Number of occurences")
-popAxes.set_xticks(popBins) # force display tick at every step of 5
+    # figure and plotting
+    distribFig, distribAxes = plt.subplots() #1 row, 1 col
+    distribFig.set_size_inches(pdfPageSize)
+    counts, edges, patches = distribAxes.hist(distribDf.to_numpy(), bins=distribBins, color=color)
 
-pdf.savefig(popFig)
+    # bar labels
+    # plt.bar_label(bars) (bars being the 3rd return value of hist()) can't be used.
+    # Bars on the side are very wide due to going all the way down/up to min/max.
+    # To make them loop equally sized, the chart is cropped on the side.
+    # However labels are still centered by bar_label, and thus the labels for the first
+    # and last bars are out of bound. Thus, it has to be done manually
+    spacing = 0.01 * max(counts)
+    # the first bar label is calculated from the right, not the left, as the left is far beyond the chart boundaries.
+    distribAxes.text(distribBins[1] - rStep/2, counts[0] + spacing, int(counts[0]), ha="center") # x, y, value
+    for i in range(1, len(counts)):
+        distribAxes.text(distribBins[i] + rStep/2, counts[i] + spacing, int(counts[i]), ha="center")
+    
+    # additional information
+    distribAxes.set_title(name)
+    finalXLabel = distribDataColumn if (xLabel == "") else xLabel
+    if (not unit == ""):
+        finalXLabel += f" ({unit})"
+    distribAxes.set_xlabel(distribDataColumn)
+    distribAxes.set_ylabel("Number of occurences")
+    distribAxes.set_xlim(rMin - rStep, rMax + rStep)
+
+    # tweak x axis ticks
+    # scaling first (scaling the ticks is sufficient for the chart to remain accurate, and is simpler)
+    xTicks = distribBins[1:-1] * scale # exclude min and max added above, they should not be displayed.
+    xTicks = xTicks.astype(str) 
+    # then adding text for indicating above n and below n more explicitely
+    # xTicks[0] = f"<{xTicks[0]} | {xTicks[0]}"
+    # xTicks[-1] = f"{xTicks[-1]} | >{xTicks[-1]}"
+    distribAxes.text(distribBins[1] - rStep/2, -8 * spacing, "below", ha="center")
+    distribAxes.text(distribBins[-2] + rStep/2, -8 * spacing, "above", ha="center")
+    # and finally modifying ticks
+    distribAxes.set_xticks(distribBins[1:-1]) # Create FixedLocator, otherwise set_xticklabels raise a warning and ignore new labels
+    distribAxes.set_xticklabels(xTicks) # force display tick at every step
+
+    # see also: https://stackoverflow.com/questions/26218704/matplotlib-histogram-with-collection-bin-for-high-values
+
+    pdf.savefig(distribFig)
+
+
+# Distributions
+
+# Popularity
+buildDistribution("How popular are the tracks of your playlist?\n(0 are niche, 100 are most streamed tracks)",
+        Columns.POPULARITY,
+        rMin=0, rMax=100, rStep=5)
+
+# Duration
+buildDistribution("How long are the tracks?\n",
+        Columns.DURATION,
+        rMin=60_000, rMax=60_000*10, rStep=30_000,
+        scale=(1/60000),
+        xLabel="Duration",
+        unit="minutes",
+        color="#e83c3f")
 
 
 
@@ -214,34 +258,84 @@ pdf.savefig(popFig)
 # RANKINGS (between 0 and 100)
 # ========
 
+print("- rankings")
 addTitle("Rankings")
 
-# ranking by popularity
-# ---------------------
+def buildRanking(name: str, sortingColumn: str, maxRows: int=20, least: bool=False, scale: int=1, unit: str="", xLabel: str="", color: str="C0"):
+    """
+    Builds an horizontal bar chart with the given name,
+    displaying a list of highest or lowest values of a given column.
+    Labels contain the index and name of the selected tracks.
+    
+    Parameters:
+    - `name`: name of the figure.
+    - `sortingColumn`: the dataFrame column to use in `dataFrame`.
+    - `maxRows`: The maximum of tracks displayed in the figure.
+    - `least`: Sort by lowest values instead of highest values.
+    - `scale`: can be used to apply a factor to the ranked column (on a dataframe copy).
+    - `unit`: String to display a unit next to the axis label.
+    - `xlabel`: override the label for the x axis with a custom one, instead of the column name.
+    - `color`: Sets the bars color
+    """
+    # setup and style
+    rankFig, rankAxes = plt.subplots()
+    rankFig.set_size_inches(pdfPageSize)
+    rankFig.subplots_adjust(left=0.50)
 
-print("- rankings")
-# setup and style
-mostPopRankFig, mostPopRankAxes = plt.subplots()
-mostPopRankFig.set_size_inches(pdfPageSize)
-mostPopRankFig.subplots_adjust(left=0.50)
+    # get ranking
+    rankDf = dataFrame[[Columns.TRACK_NAME, sortingColumn]].copy()
+    # scaling
+    rankDf[sortingColumn] *= scale
+    if (not least):
+        # most
+        rankDf = rankDf.sort_values(sortingColumn).tail(maxRows)
+    else:
+        # least
+        #[::-1] reverses the list, see https://www.codingem.com/reverse-slicing-in-python/
+        rankDf = rankDf.sort_values(sortingColumn).head(maxRows).iloc[::-1]
 
-# get ranking
-mostPopRankDf = dataFrame[[Columns.TRACK_NAME, Columns.POPULARITY]]
-mostPopRankDf = mostPopRankDf.sort_values(Columns.POPULARITY).tail(20)
+    # build list of labels and values
+    rankLabels = []
+    STR_MAX_LEN = 50
+    for index, row in rankDf.iterrows():
+        label = f"{index} - {row[Columns.TRACK_NAME]}"
+        if len(label) > STR_MAX_LEN:
+            label = label[0:STR_MAX_LEN] + "..."
+        rankLabels.append(label)
+    rankValues = rankDf[sortingColumn].to_numpy()
 
-# build list of labels and values
-mostPopRankLabels = []
-for index, row in mostPopRankDf.iterrows():
-    mostPopRankLabels.append(f"{index} - {row[Columns.TRACK_NAME]}")
-mostPopRankValues = mostPopRankDf[Columns.POPULARITY].to_numpy()
+    # create plot and append information
+    barContainer = rankAxes.barh(rankLabels, rankValues, color=color) # inverts order of dataframe on display
+    rankAxes.bar_label(barContainer, padding=2)
+    rankAxes.set_title(name)
+    label = sortingColumn if (xLabel == "") else xLabel
+    if (not unit == ""):
+        label += f" ({unit})"
+    rankAxes.set_xlabel(label)
 
-# plot and labels
-barContainer = mostPopRankAxes.barh(mostPopRankLabels, mostPopRankValues) # inverts order of dataframe on display
-mostPopRankAxes.bar_label(barContainer, padding=2)
-mostPopRankAxes.set_title("Most popular tracks")
-mostPopRankAxes.set_xlabel("Popularity")
+    pdf.savefig(rankFig)
 
-pdf.savefig(mostPopRankFig)
+
+# Rankings
+
+# Popularity
+buildRanking("Most popular tracks", Columns.POPULARITY)
+buildRanking("Least popular tracks", Columns.POPULARITY, least=True)
+
+#Duration
+buildRanking("Longest tracks",
+        Columns.DURATION,
+        scale=(1/60000),
+        xLabel="Duration",
+        unit="minutes",
+        color="#e83c3f")
+buildRanking("Shortest tracks",
+        Columns.DURATION,
+        least=True,
+        scale=(1/60000),
+        xLabel="Duration",
+        unit="minutes",
+        color="#e83c3f")
 
 
 
